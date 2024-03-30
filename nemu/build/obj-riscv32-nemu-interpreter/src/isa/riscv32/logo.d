71bb2ae8c421224440aec06cde0dde8ff721e912
WangQiZ <wanggqqz@gmail.com>
2024-03-30 21:44:56 +0800

    init

--- nemu/build/obj-riscv32-nemu-interpreter/src/isa/riscv32/logo.d
+++
@@ -1,9 +0,0 @@
-cmd_/home/wqz/code/ics-pa-2022/nemu/build/obj-riscv32-nemu-interpreter/src/isa/riscv32/logo.o := unused
-
-source_/home/wqz/code/ics-pa-2022/nemu/build/obj-riscv32-nemu-interpreter/src/isa/riscv32/logo.o := src/isa/riscv32/logo.c
-
-deps_/home/wqz/code/ics-pa-2022/nemu/build/obj-riscv32-nemu-interpreter/src/isa/riscv32/logo.o := \
-
-/home/wqz/code/ics-pa-2022/nemu/build/obj-riscv32-nemu-interpreter/src/isa/riscv32/logo.o: $(deps_/home/wqz/code/ics-pa-2022/nemu/build/obj-riscv32-nemu-interpreter/src/isa/riscv32/logo.o)
-
-$(deps_/home/wqz/code/ics-pa-2022/nemu/build/obj-riscv32-nemu-interpreter/src/isa/riscv32/logo.o):
